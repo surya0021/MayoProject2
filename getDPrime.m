83cc3286aa403e76d4933f71f80b4314a2ba8f98
surya0021 <surya.0021@gmail.com>
2022-06-29 13:29:11 +0530

    uses pooled variance and omits nan values

--- getDPrime.m
+++ getDPrime.m
@@ -1,4 +1,5 @@
 function d = getDPrime(x1,x2)
-stdVal = sqrt((var(x1)+var(x2))/2);
-d = (mean(x1)- mean(x2))/stdVal;
+n1 = length(x1);    n2 = length(x2);
+stdVal = sqrt(((n1-1)*var(x1,'omitnan')+(n2-1)*var(x2,'omitnan'))/(n1+n2-2));
+d = (mean(x1,'omitnan')- mean(x2,'omitnan'))/stdVal;
 end
